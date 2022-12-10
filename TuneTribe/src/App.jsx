8c1807d4adf905478c18c3809d3cdc84714b5300
jonrob <jonrob08@gmail.com>
2022-12-10 13:29:08 -0700

    Set up initial file/folder structure
    I'm exporting all files in an index.js file so that it can be imported much cleaner in the main App.jsx file. Also added the sidebar to the app.

--- TuneTribe/src/App.jsx
+++ TuneTribe/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
-
+import { Searchbar, Sidebar } from './components';
+import { DiscoverArtists } from './pages';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="relative flex">
+      <Sidebar />
+    </div>
   )
 }
 

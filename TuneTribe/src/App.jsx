5d8511931affc9b57cf39daf998aef7b3575c12a
jonrob <jonrob08@gmail.com>
2022-12-10 13:43:40 -0700

    Styling updates
    added Tailwind base, utils, and components to App CSS file, removed all unused starter CSS.

--- TuneTribe/src/App.jsx
+++ TuneTribe/src/App.jsx
@@ -7,6 +7,17 @@ const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        <Searchbar />
+
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+            <Routes>
+              <Route path="/" element={<DiscoverArtists />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
